8693746271590a682f5379cc529ffcd3cb9d5bdd
pow2443 <jaro2443@uw.edu>
2018-03-04 21:59:22 -0800

    monopoly game

---
+++ Assignment 3.Designer.vb
@@ -0,0 +1,237 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Assignment3
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnPlayer1 = New System.Windows.Forms.Button()
+        Me.lblDirection1 = New System.Windows.Forms.Label()
+        Me.btnPlayer2 = New System.Windows.Forms.Button()
+        Me.lblDirection3 = New System.Windows.Forms.Label()
+        Me.lblDirection2 = New System.Windows.Forms.Label()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.picPlayer2 = New System.Windows.Forms.PictureBox()
+        Me.picPlayer1 = New System.Windows.Forms.PictureBox()
+        Me.lblID = New System.Windows.Forms.Label()
+        Me.lblTime = New System.Windows.Forms.Label()
+        CType(Me.picPlayer2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picPlayer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnPlayer1
+        '
+        Me.btnPlayer1.BackColor = System.Drawing.Color.Tomato
+        Me.btnPlayer1.Font = New System.Drawing.Font("Trebuchet MS", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPlayer1.ForeColor = System.Drawing.Color.White
+        Me.btnPlayer1.Location = New System.Drawing.Point(140, 320)
+        Me.btnPlayer1.Name = "btnPlayer1"
+        Me.btnPlayer1.Size = New System.Drawing.Size(101, 36)
+        Me.btnPlayer1.TabIndex = 6
+        Me.btnPlayer1.Text = "Dice of player1"
+        Me.btnPlayer1.UseVisualStyleBackColor = False
+        '
+        'lblDirection1
+        '
+        Me.lblDirection1.AutoSize = True
+        Me.lblDirection1.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDirection1.Location = New System.Drawing.Point(165, 281)
+        Me.lblDirection1.Name = "lblDirection1"
+        Me.lblDirection1.Size = New System.Drawing.Size(50, 14)
+        Me.lblDirection1.TabIndex = 3
+        Me.lblDirection1.Text = "Player 1"
+        '
+        'btnPlayer2
+        '
+        Me.btnPlayer2.BackColor = System.Drawing.Color.SlateBlue
+        Me.btnPlayer2.Font = New System.Drawing.Font("Trebuchet MS", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPlayer2.ForeColor = System.Drawing.Color.White
+        Me.btnPlayer2.Location = New System.Drawing.Point(387, 320)
+        Me.btnPlayer2.Name = "btnPlayer2"
+        Me.btnPlayer2.Size = New System.Drawing.Size(105, 36)
+        Me.btnPlayer2.TabIndex = 7
+        Me.btnPlayer2.Text = "Dice of player2"
+        Me.btnPlayer2.UseVisualStyleBackColor = False
+        '
+        'lblDirection3
+        '
+        Me.lblDirection3.AutoSize = True
+        Me.lblDirection3.Font = New System.Drawing.Font("Stencil", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDirection3.Location = New System.Drawing.Point(252, 253)
+        Me.lblDirection3.Name = "lblDirection3"
+        Me.lblDirection3.Size = New System.Drawing.Size(129, 18)
+        Me.lblDirection3.TabIndex = 2
+        Me.lblDirection3.Text = "Number of dice"
+        '
+        'lblDirection2
+        '
+        Me.lblDirection2.AutoSize = True
+        Me.lblDirection2.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDirection2.Location = New System.Drawing.Point(414, 281)
+        Me.lblDirection2.Name = "lblDirection2"
+        Me.lblDirection2.Size = New System.Drawing.Size(50, 14)
+        Me.lblDirection2.TabIndex = 4
+        Me.lblDirection2.Text = "Player 2"
+        '
+        'lblOutput
+        '
+        Me.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblOutput.Font = New System.Drawing.Font("Stencil", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOutput.Location = New System.Drawing.Point(285, 281)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(66, 57)
+        Me.lblOutput.TabIndex = 5
+        Me.lblOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Font = New System.Drawing.Font("Showcard Gothic", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.lblName.Location = New System.Drawing.Point(210, 146)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(219, 40)
+        Me.lblName.TabIndex = 0
+        Me.lblName.Text = "Candy Land"
+        '
+        'lblResult
+        '
+        Me.lblResult.Font = New System.Drawing.Font("Snap ITC", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResult.ForeColor = System.Drawing.Color.MediumTurquoise
+        Me.lblResult.Location = New System.Drawing.Point(140, 199)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(352, 36)
+        Me.lblResult.TabIndex = 1
+        Me.lblResult.Text = "none"
+        Me.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.Gainsboro
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Font = New System.Drawing.Font("Trebuchet MS", 9.0!)
+        Me.btnExit.ForeColor = System.Drawing.Color.Gray
+        Me.btnExit.Location = New System.Drawing.Point(275, 444)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(86, 26)
+        Me.btnExit.TabIndex = 9
+        Me.btnExit.Text = "&Surrender"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.btnClear.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnClear.Font = New System.Drawing.Font("Trebuchet MS", 9.0!)
+        Me.btnClear.ForeColor = System.Drawing.Color.Gray
+        Me.btnClear.Location = New System.Drawing.Point(275, 412)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(86, 26)
+        Me.btnClear.TabIndex = 8
+        Me.btnClear.Text = "&New Game"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'picPlayer2
+        '
+        Me.picPlayer2.Image = Global.Assignment_3.My.Resources.Resources._2
+        Me.picPlayer2.Location = New System.Drawing.Point(-1, -1)
+        Me.picPlayer2.Name = "picPlayer2"
+        Me.picPlayer2.Size = New System.Drawing.Size(29, 65)
+        Me.picPlayer2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.picPlayer2.TabIndex = 10
+        Me.picPlayer2.TabStop = False
+        '
+        'picPlayer1
+        '
+        Me.picPlayer1.Image = Global.Assignment_3.My.Resources.Resources._1
+        Me.picPlayer1.Location = New System.Drawing.Point(28, -1)
+        Me.picPlayer1.Name = "picPlayer1"
+        Me.picPlayer1.Size = New System.Drawing.Size(36, 65)
+        Me.picPlayer1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picPlayer1.TabIndex = 9
+        Me.picPlayer1.TabStop = False
+        '
+        'lblID
+        '
+        Me.lblID.AutoSize = True
+        Me.lblID.Location = New System.Drawing.Point(415, 71)
+        Me.lblID.Name = "lblID"
+        Me.lblID.Size = New System.Drawing.Size(158, 36)
+        Me.lblID.TabIndex = 11
+        Me.lblID.Text = "<Hyeongwoo Park>" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Assigment 3 CS 115<2871>" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "<Winter><2015>"
+        '
+        'lblTime
+        '
+        Me.lblTime.AutoSize = True
+        Me.lblTime.Location = New System.Drawing.Point(415, 107)
+        Me.lblTime.Name = "lblTime"
+        Me.lblTime.Size = New System.Drawing.Size(0, 12)
+        Me.lblTime.TabIndex = 12
+        '
+        'Assignment3
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.BackColor = System.Drawing.Color.PaleGoldenrod
+        Me.CancelButton = Me.btnExit
+        Me.ClientSize = New System.Drawing.Size(641, 650)
+        Me.Controls.Add(Me.lblTime)
+        Me.Controls.Add(Me.lblID)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.picPlayer2)
+        Me.Controls.Add(Me.picPlayer1)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.lblDirection2)
+        Me.Controls.Add(Me.lblDirection3)
+        Me.Controls.Add(Me.btnPlayer2)
+        Me.Controls.Add(Me.lblDirection1)
+        Me.Controls.Add(Me.btnPlayer1)
+        Me.Name = "Assignment3"
+        Me.Text = "Board Game"
+        CType(Me.picPlayer2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picPlayer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnPlayer1 As System.Windows.Forms.Button
+    Friend WithEvents lblDirection1 As System.Windows.Forms.Label
+    Friend WithEvents btnPlayer2 As System.Windows.Forms.Button
+    Friend WithEvents lblDirection3 As System.Windows.Forms.Label
+    Friend WithEvents lblDirection2 As System.Windows.Forms.Label
+    Friend WithEvents lblOutput As System.Windows.Forms.Label
+    Friend WithEvents lblName As System.Windows.Forms.Label
+    Friend WithEvents lblResult As System.Windows.Forms.Label
+    Friend WithEvents picPlayer1 As System.Windows.Forms.PictureBox
+    Friend WithEvents picPlayer2 As System.Windows.Forms.PictureBox
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents lblID As System.Windows.Forms.Label
+    Friend WithEvents lblTime As System.Windows.Forms.Label
+
+End Class
---
+++ Assignment 3.vb
@@ -0,0 +1,314 @@
+﻿Option Strict On
+Option Explicit On
+'----------------------------------------------------------
+' Assignment (Candy Land)
+' by <Hyeongwoo Park>
+' version 1.0
+' Board Game
+'----------------------------------------------------------
+Public Class Assignment3
+
+    Const intSQUARE_SIZE As Integer = 65
+    Dim lblArray(35) As Label                   'array of labels that make up the game board
+    Dim intRules(35) As Integer                 'parallel array containing rules for the squares corresponding to lblArray entries
+    Dim rand As New Random                      'random
+    Dim dice As Integer                         'Saving number of a dice       
+    Dim initP1Position, initP2Position As New System.Drawing.Point(0, 0) 'Saving initial location to each picture
+    Dim P1P2PosDistX As Integer                 'the value for when the pictures is on same place
+    Dim p1Position As Integer                   'position of player 1 on board
+    Dim p2Position As Integer                   'position of player 2 on board
+    Dim isGameOver As Boolean = False           'for exit
+
+
+    Private Function Random(ByRef dieSize As Integer) As Integer
+        'Return a random integer in the range of 'size'
+        Return rand.Next(dieSize) + 1
+
+    End Function
+
+    Private Sub Assignment3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'player1 starts first
+        btnPlayer2.Enabled = False
+
+        'clear the game result label
+        lblResult.Text = ""
+
+        'Output Time
+        lblTime.Text = (DateString & " " & TimeString)
+
+        'gets initial player markers locations
+        initP1Position = picPlayer1.Location
+        initP2Position = picPlayer2.Location
+        'gets initial player markers distance in x
+        P1P2PosDistX = initP1Position.X - initP2Position.X
+
+
+        
+        Dim intCount As Integer
+
+        'This for..Next Loop sets properties of each label in the array. The Select..Case statement
+        'places the labels around the edges of the form 
+        For intCount = 0 To (lblArray.Length - 1)
+
+            lblArray(intCount) = New Label
+            lblArray(intCount).Size = New System.Drawing.Size(intSQUARE_SIZE, intSQUARE_SIZE)
+            lblArray(intCount).BorderStyle = BorderStyle.FixedSingle
+            lblArray(intCount).Name = "lbl" & intCount
+            lblArray(intCount).Visible = True
+
+            'The following line display the array index on each square
+            If intCount = 0 Then
+
+                lblArray(intCount).Text = "Start"
+
+
+            ElseIf intCount = 35 Then
+
+                lblArray(intCount).Text = "Finish Line"
+
+            Else
+
+                lblArray(intCount).Text = intCount.ToString
+
+            End If
+
+
+            Select Case intCount
+
+                Case Is < 10
+                    lblArray(intCount).Location = New System.Drawing.Point(intSQUARE_SIZE * intCount, 0)
+                Case 10 To 18
+                    lblArray(intCount).Location =
+                        New System.Drawing.Point(intSQUARE_SIZE * 9, intSQUARE_SIZE * (intCount - 9))
+                Case 19 To 27
+                    lblArray(intCount).Location =
+                        New System.Drawing.Point(intSQUARE_SIZE * 9 - intSQUARE_SIZE * (intCount - 18),
+                                                 intSQUARE_SIZE * 9)
+                Case 28 To 35
+                    lblArray(intCount).Location =
+                        New System.Drawing.Point(0, intSQUARE_SIZE * 9 - intSQUARE_SIZE * (intCount - 27))
+
+            End Select
+
+
+            ' Set colors of the labels
+            Select Case intCount Mod 6
+                Case 0
+                    lblArray(intCount).BackColor = Color.MistyRose
+
+                Case 1
+                    lblArray(intCount).BackColor = Color.Yellow
+
+                Case 2
+                    lblArray(intCount).BackColor = Color.DeepSkyBlue
+
+                Case 3
+                    lblArray(intCount).BackColor = Color.Violet
+
+                Case 4
+                    lblArray(intCount).BackColor = Color.Lime
+
+                Case 5
+
+                    lblArray(intCount).BackColor = Color.Salmon
+
+            End Select
+
+            Controls.Add(lblArray(intCount))
+
+        Next
+
+
+        'Initialize special squares
+        SetRules()
+
+    End Sub
+
+    'Set the rules to intRules
+    Private Sub SetRules()
+
+        lblArray(7).Text = "2 steps back"
+        intRules(7) = -2
+
+        'a new rule added
+        lblArray(9).Text = "Airplane!"
+        intRules(9) = Random(6)
+
+        lblArray(13).Text = "1 step forward"
+        intRules(13) = 1
+
+        lblArray(19).Text = "2 steps forward"
+        intRules(19) = 2
+
+        lblArray(32).Text = "1 step back"
+        intRules(32) = -1
+
+    End Sub
+
+
+    'Do math to move to new position
+    Sub moving(ByRef movement As Integer, ByRef pic As PictureBox)
+
+        'clear system label
+        lblResult.Text = ""
+
+
+        'Check the rule of position 
+        'and apply the rule to player!
+        If movement = 7 Then
+            movement = movement + intRules(7)
+            lblResult.Text = pic.Name.Substring(3) & " " & lblArray(7).Text
+        ElseIf movement = 9 Then
+            movement = movement + intRules(9)
+            lblResult.Text = pic.Name.Substring(3) & " " & lblArray(9).Text
+        ElseIf movement = 13 Then
+            movement = movement + intRules(13)
+            lblResult.Text = pic.Name.Substring(3) & " " & lblArray(13).Text
+        ElseIf movement = 19 Then
+            movement = movement + intRules(19)
+            lblResult.Text = pic.Name.Substring(3) & " " & lblArray(19).Text
+        ElseIf movement = 32 Then
+            movement = movement + intRules(32)
+            lblResult.Text = pic.Name.Substring(3) & " " & lblArray(32).Text
+        End If
+
+        'moving the picture to new position
+        Select Case movement
+            Case Is < 10
+                pic.Location = New System.Drawing.Point(intSQUARE_SIZE * movement, 0)
+            Case 10 To 18
+                pic.Location =
+                    New System.Drawing.Point(intSQUARE_SIZE * 9, intSQUARE_SIZE * (movement - 9))
+            Case 19 To 27
+                pic.Location =
+                    New System.Drawing.Point(intSQUARE_SIZE * 9 - intSQUARE_SIZE * (movement - 18),
+                                             intSQUARE_SIZE * 9)
+            Case 28 To 35
+                pic.Location =
+                    New System.Drawing.Point(0, intSQUARE_SIZE * 9 - intSQUARE_SIZE * (movement - 27))
+        End Select
+
+        'always player1 right align 
+        If pic.Name = "picPlayer1" Then
+            pic.Left += P1P2PosDistX
+        End If
+
+    End Sub
+
+    'To exit 
+    Sub GameOver()
+        btnPlayer1.Enabled = False
+        btnPlayer2.Enabled = False
+        isGameOver = True
+    End Sub
+
+
+    Private Sub btnPlayer1_Click(sender As Object, e As EventArgs) Handles btnPlayer1.Click
+
+        'Cast a dice
+        'Output the number of dice
+        dice = Random(6)
+        lblOutput.Text = dice.ToString
+
+        'Save the position
+        p1Position = p1Position + dice
+
+        'check the position of player1
+        'if the position is not over 35, keep going the game
+        If p1Position < 35 Then
+            moving(p1Position, picPlayer1)
+
+        ElseIf p1Position = 35 Then
+            btnPlayer1.Enabled = False
+            btnPlayer2.Enabled = False
+            moving(p1Position, picPlayer1)
+            lblResult.Text = "Player1's Win"
+            'focus exit button
+            btnExit.Focus()
+            'disable die buttons
+            GameOver()
+
+        ElseIf p1Position >= 36 Then
+            p1Position = p1Position - dice
+
+        End If
+
+        If Not isGameOver Then
+            'switches turn
+            btnPlayer1.Enabled = False
+            btnPlayer2.Enabled = True
+        End If
+        'focus player 2 button
+        btnPlayer2.Focus()
+
+    End Sub
+
+
+    Private Sub btnPlayer2_Click(sender As Object, e As EventArgs) Handles btnPlayer2.Click
+
+        'Cast a dice
+        'Output the number of dice
+        dice = Random(6)
+        lblOutput.Text = dice.ToString
+
+
+        'Save the position
+        p2Position = p2Position + dice
+
+
+        'check the position of player1
+        'if the position is not over 35, keep going the game
+        If p2Position < 35 Then
+            moving(p2Position, picPlayer2)
+
+        ElseIf p2Position = 35 Then
+
+            btnPlayer1.Enabled = False
+            btnPlayer2.Enabled = False
+            moving(p2Position, picPlayer2)
+            lblResult.Text = "Player2's Win"
+            'focus exit button
+            btnExit.Focus()
+            'disable die buttons
+            GameOver()
+
+        ElseIf p2Position > 35 Then
+
+            p2Position = p2Position - dice
+
+        End If
+
+        If Not isGameOver Then
+            'switches turn
+            btnPlayer1.Enabled = True
+            btnPlayer2.Enabled = False
+        End If
+
+        'focus player 2 button
+        btnPlayer1.Focus()
+    End Sub
+
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'initializes and resets
+        isGameOver = False
+        p1Position = 0
+        p2Position = 0
+        picPlayer1.Location = initP1Position
+        picPlayer2.Location = initP2Position
+        btnPlayer1.Enabled = True
+        btnPlayer2.Enabled = False
+        lblOutput.Text = ""
+        lblResult.Text = ""
+    End Sub
+
+
+    'Close the program
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+End Class
+
+
